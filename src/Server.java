0ac48cf6a91506a39f0573b127c14df1cd2fd434
Yosra-Oueslati <62852513+yosra2001@users.noreply.github.com>
2021-12-11 21:47:21 +0100

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -14,4 +14,48 @@ public class Server extends UnicastRemoteObject implements MyInterface {
 
         Naming.rebind("rmi://127.0.0.1:1250/server", server);
     }
+    
+    public String Reverse (String s1)
+    {
+        String s2 ="";
+        char ch;
+        for (int i=0; i< s1.length(); i++)
+        {
+            ch= s1.charAt(i);
+            s2= ch+s2 ;
+        }
+      return s2;
+    }
+
+    @Override
+    public char minChar(String s1)
+    {
+        char c =s1.charAt(0);
+        for (int i=1; i<s1.length(); i++)
+        {
+            if(s1.charAt(i) < c)
+            {
+                c=s1.charAt(i);
+            }
+        }
+        return c;
+    }
+    public String upperLower ( String s1)
+    {
+        String s2 ="";
+        for (int i = 0; i <s1.length() ; i++) {
+        if (s1.charAt(i)>='a'&& s1.charAt(i)<='z')
+        {
+            s2=s2+(char)(s1.charAt(i)-32);
+        }
+        else if  (s1.charAt(i)>='A'&& s1.charAt(i)<='Z')
+        {
+            s2=s2+(char)(s1.charAt(i)+32);
+        }
+        else {
+            s2=s2+(char)(s1.charAt(i));
+        }
+        }
+        return s2;
+    }
 }
