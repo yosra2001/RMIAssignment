de3ab42149a646fdfc0abce5f2976a2bc915775a
Yosra-Oueslati <62852513+yosra2001@users.noreply.github.com>
2021-12-11 21:47:41 +0100

    Update MyInterface.java

--- src/MyInterface.java
+++ src/MyInterface.java
@@ -2,5 +2,8 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface MyInterface extends Remote {
-
+  public String Reverse(String s1) throws RemoteException;
+    public char minChar(String s1) throws RemoteException;
+    public String upperLower(String s1) throws RemoteException;
+}
 }
